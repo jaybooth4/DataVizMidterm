b996e9c7c7154de57e040ce5164d4146c4eb2313
Jason Booth <booth.j@husky.neu.edu>
2019-03-08 15:38:59 -0500

    Fixed embeddings to return sparse matrix and id2word dictionary

--- Pipeline.py
+++ Pipeline.py
@@ -1,18 +1,31 @@
 from backend.Embeddings import getEmbeddings
 from backend.Preprocess import preprocess
 from backend.Util import createSparkContext, loadData
+from backend.ML import clusterData
 from sklearn.datasets import fetch_20newsgroups
+from gensim.matutils import Sparse2Corpus
 import argparse
+import numpy as np
 
 
 def main():
-    sc = createSparkContext()
-    corpus, labels = preprocess("NLTK", 100, sc)#, save=True)
-    # corpus, labels = loadData("preprocess")
-    # bow, tfidf, doc2vecFormat = getEmbeddings(corpus, labels, sc)
-
-    print(corpus[:20])
-    print(fetch_20newsgroups(subset='train', remove=('headers', 'footers', 'quotes'), download_if_missing=True).data[:20])
+    # sc = createSparkContext()
+    # corpus, labels = preprocess("NLTK", 100, sc, save=True)
+    corpus, labels = loadData("preprocess")
+    bow, tfidf, doc2vecFormat, id2word = getEmbeddings(corpus, labels)
+    # for doc in corpus:
+    #     print(doc)
+    # print(type(bow.todense()))
+    # print(bow.todense().shape)
+    # np.apply_along_axis( lambda row: print(np.sum(row)), axis=1, arr=bow.todense() )
+    # print(dir(bow.todense()))
+    # for doc in bow.todense():
+    #     print(doc)
+    #     print([id2word[entry[0]] for entry in doc])
+    # print(bow)
+    # for doc in bow:
+    #     print(doc)
+    # print(dir(tfidf))
 
 
     # parser = argparse.ArgumentParser(description = 'Data Visualization Project Pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
--- backend/Embeddings.py
+++ backend/Embeddings.py
@@ -1,50 +1,37 @@
 from collections import Counter
 from .Util import parallelizeData
-import uuid 
+import uuid
 import math
 from operator import add
+from gensim.matutils import Sparse2Corpus
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
-def bagOfWordsFormat(dataRdd):
-    return dataRdd.map(lambda tokens: list(Counter(tokens).items()))
+def bagOfWordsFormat(data):
+    # Count vectorizer only accepts a list of strings, not a list of lists of tokens
+    vectorizerData = list(map(lambda tokens: " . ".join(tokens), data))
+    # print(vectorizerData)
+    countVectorizer = CountVectorizer(preprocessor=None, tokenizer=None, stop_words=None)
+    bow = countVectorizer.fit_transform(vectorizerData)
+    # print("Feature names")
+    # print(countVectorizer.get_feature_names())
+    print(countVectorizer.vocabulary_)
+    id2word = dict((id, word)
+                   for word, id in countVectorizer.vocabulary_.items())
+    return bow, id2word
 
-def termFrequency(rdd):
-    return rdd.flatMap(lambda tokens: tokens) \
-        .map(lambda word: (word, 1)) \
-        .reduceByKey(add)
 
-def documentFrequency(rdd):
-    return rdd.map(lambda doc: (str(uuid.uuid4()), doc))\
-                    .flatMapValues(lambda doc: doc)\
-                    .distinct() \
-                    .map(lambda entry: (entry[1], 1.0)) \
-                    .reduceByKey(add)
+def TFIDFFormat(bowRep):
+    tfidf = TfidfTransformer()
+    return tfidf.fit_transform(bowRep)
 
-def inverseDocumentFrequency(docFrequency):
-    ''' Expects input RDD of the format (word, count) '''
-    docFrequency.cache()
-    corpusCount = docFrequency.count()
-    idf = docFrequency.mapValues(lambda count: math.log(corpusCount / count))
-    docFrequency.unpersist()
-    return idf
-
-def TFIDF(termFreqRdd, idfRdd):
-    return termFreqRdd.join(idfRdd) \
-        .mapValues(lambda tfidf: tfidf[0] * tfidf[1])
-
-def TFIDFFormat(data):
-    tfRdd = termFrequency(data)
-    dfRdd = documentFrequency(data)
-    idfRdd = inverseDocumentFrequency(dfRdd)
-    return TFIDF(tfRdd, idfRdd)
 
 def doc2VecFormat(corpus, labels):
-    return [[corpus[i], labels[i]] for i in range(len(corpus))]        
+    return [(corpus[i], labels[i]) for i in range(len(corpus))]
 
-def getEmbeddings(data, labels, sc):
-    dataRdd = parallelizeData(data, sc).cache()
-    bow = bagOfWordsFormat(dataRdd).collect()
-    tfidf = TFIDFFormat(dataRdd).collect()
+
+def getEmbeddings(data, labels):
+    bow, id2word = bagOfWordsFormat(data)
+    tfidf = TFIDFFormat(bow)
     doc2Vec = doc2VecFormat(data, labels)
-    dataRdd.unpersist()
-    return bow, tfidf, doc2Vec
+    return bow, tfidf, doc2Vec, id2word

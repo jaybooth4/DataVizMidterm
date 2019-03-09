724458662c64327222c85a6427316cb6b04e8ad3
Jason Booth <booth.j@husky.neu.edu>
2019-03-09 12:39:04 -0500

    Full preprocessing results on dataset

--- Pipeline.py
+++ Pipeline.py
@@ -1,7 +1,7 @@
 from backend.Embeddings import getEmbeddings
 from backend.Preprocess import preprocess
 from backend.Util import createSparkContext, loadData
-from backend.ML import trainLDA, trainDoc2Vec, clusterData, clusterDataMiniBatch
+from backend.ML import trainLDA, trainDoc2Vec, clusterDataMiniBatch, getLDARep, getDoc2VecRep
 from backend.Stats import getStats
 from frontend.termfrequency_vs_importance_grapher import graphTFVsImp
 from frontend.kmeansclustering_grapher import graphKMeansClusters
@@ -9,6 +9,7 @@ from sklearn.datasets import fetch_20newsgroups
 from gensim.matutils import Sparse2Corpus
 import argparse
 import numpy as np
+import time
 
 
 def main():
@@ -17,7 +18,7 @@ def main():
 
     # Preprocess
     # corpus, labels = preprocess("NLTK", sc, save=True, name="nltk")
-    # corpus, labels = loadData("preprocess-nltk")
+    corpus, labels = loadData("preprocess-nltk")
 
     # corpus, labels = preprocess("Spacy", sc, save=True, name="spacy")
     # corpus, labels = loadData("preprocess-spacy")
@@ -32,23 +33,68 @@ def main():
 
 
     #ML
-    # bow, tfidf, doc2VecFormat, id2word = loadData("embeddings-nltk")
+    bow, tfidf, doc2VecFormat, id2word = loadData("embeddings-nltk")
     # ldaModelBow = trainLDA(bow, id2word, save=True, name="nltk-bow")
-    # print("lda bow")
     # ldaModelTfidf = trainLDA(tfidf, id2word, save=True, name="nltk-tfidf")
-    # print("lda tfidf")
-    # Doc2VecModel = trainDoc2Vec(doc2VecFormat, save=True, name="nltk")
-    # print("doc2vec")
-
-    bow, tfidf, doc2VecFormat, id2word = loadData("embeddings-spacy")
-    ldaModelBow = trainLDA(bow, id2word, save=True, name="spacy-bow")
-    print("lda bow")
-    ldaModelTfidf = trainLDA(tfidf, id2word, save=True, name="spacy-tfidf")
-    print("lda tfidf")
-    Doc2VecModel = trainDoc2Vec(doc2VecFormat, save=True, name="spacy")
-    print("doc2vec")
+    # doc2VecModel = trainDoc2Vec(corpus, save=True, name="nltk")
     
-    # kmeansLabels, nmi = clusterData(bow, labels, save=True)
+    # ldaModelBow = loadData("ldamodel-nltk-bow")
+    # ldaRepBow = getLDARep(ldaModelBow, bow, save=True, name="ldarep-nltk-bow")
+    # ldaModelTfidf = loadData("ldamodel-nltk-tfidf")
+    # ldaReptfidf = getLDARep(ldaModelTfidf, tfidf, save=True, name="ldarep-nltk-tfidf")
+
+    # doc2VecModel = loadData("doc2vec-nltk")
+    # doc2VecRep = getDoc2VecRep(doc2VecModel, len(corpus), save=True, name="doc2vec-rep-nltk")
+
+
+    # bow, tfidf, doc2VecFormat, id2word = loadData("embeddings-spacy")
+    # ldaModelBow = trainLDA(bow, id2word, save=True, name="spacy-bow")
+    # ldaModelTfidf = trainLDA(tfidf, id2word, save=True, name="spacy-tfidf")
+    # doc2VecModel = trainDoc2Vec(corpus, save=True, name="spacy")
+    
+    # ldaModelBow = loadData("ldamodel-spacy-bow")
+    # ldaRepBow = getLDARep(ldaModelBow, bow, save=True, name="ldarep-spacy-bow")
+    # ldaModelTfidf = loadData("ldamodel-spacy-tfidf")
+    # ldaReptfidf = getLDARep(ldaModelTfidf, tfidf, save=True, name="ldarep-spacy-tfidf")
+    
+    # doc2VecModel = loadData("doc2vec-nltk")
+    # doc2vecRep = getDoc2VecRep(doc2VecModel, save=True, name="doc2vec-rep-nltk")
+
+    # doc2VecModel = loadData("doc2vec-spacy")
+    # doc2vecRep = getDoc2VecRep(doc2VecModel, save=True, name="doc2vec-rep-spacy")
+
+
+    doc2vecRepNltk = loadData("doc2vec-rep-nltk")
+    ldaRepNltkTfidf = loadData("ldarep-nltk-tfidf")
+    ldaRepNltkBow = loadData("ldarep-nltk-bow")
+    print(time.time())
+    clusterDataMiniBatch(bow, labels, save=True, name="nltk-bow")
+    print(time.time())
+    clusterDataMiniBatch(tfidf, labels, save=True, name="nltk-tfidf")
+    print(time.time())
+    clusterDataMiniBatch(ldaRepNltkTfidf, labels, save=True, name="nltk-lda-tfidf")
+    print(time.time())
+    clusterDataMiniBatch(ldaRepNltkBow, labels, save=True, name="nltk-lda-bow")
+    print(time.time())
+    clusterDataMiniBatch(doc2vecRepNltk, labels, save=True, name="nltk-doc2vec")
+    print(time.time())
+
+
+    # doc2vecRepSpacy = loadData("doc2vec-rep-spacy")
+    # ldaRepSpacyTfidf = loadData("ldarep-spacy-tfidf")
+    # ldaRepSpacyBow = loadData("ldarep-spacy-bow")
+    # print(time.time())
+    # clusterDataMiniBatch(bow, labels, save=True, name="spacy-bow")
+    # print(time.time())
+    # clusterDataMiniBatch(tfidf, labels, save=True, name="spacy-tfidf")
+    # print(time.time())
+    # clusterDataMiniBatch(ldaRepSpacyTfidf, labels, save=True, name="spacy-lda-tfidf")
+    # print(time.time())
+    # clusterDataMiniBatch(ldaRepSpacyBow, labels, save=True, name="spacy-lda-bow")
+    # print(time.time())
+    # clusterDataMiniBatch(doc2vecRepSpacy, labels, save=True, name="spacy-doc2vec")
+    # print(time.time())
+
     print("success")
 
 
--- backend/ML.py
+++ backend/ML.py
@@ -8,44 +8,68 @@ from gensim.matutils import Sparse2Corpus
 from gensim.models import Doc2Vec, ldamulticore
 from gensim.models.tfidfmodel import TfidfModel
 from gensim.test.utils import datapath, get_tmpfile
+from gensim.models.doc2vec import TaggedDocument
 from sklearn.cluster import KMeans, MiniBatchKMeans
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.metrics import normalized_mutual_info_score
 from .Util import saveData
 
+
 def trainLDA(docRep, dictionary, save=False, name=""):
     ''' Function to train and return an ldamodel. Expects a sparse matrix as input '''
     corpus = Sparse2Corpus(docRep, documents_columns=False)
-    ldamodel = ldamulticore.LdaMulticore(corpus, num_topics=20, id2word=dictionary, workers=4)
+    ldamodel = ldamulticore.LdaMulticore(
+        corpus, num_topics=20, id2word=dictionary, workers=4)
     if save:
         saveData(ldamodel, 'ldamodel-' + name)
     return ldamodel
 
 
-def trainDoc2Vec(data, epochs=50, vecSize=20, alpha=0.025, save=False, name=""):
-    ''' Train a Doc2Vec model, expects [(tokens, label)] '''
-    doc2VecModel = Doc2Vec(data, total_examples=len(data),
-                    vector_size=vecSize,
-                    alpha=alpha,
-                    min_alpha=0.00025,
-                    epochs=epochs,
-                    workers=4)
+def trainDoc2VecByTopic(data, minCount=1, vecSize=20, save=False, name=""):
+    ''' Train a Doc2Vec model to get representations of topics, expects [(tokens, topic)] '''
+    doc2VecModel = Doc2Vec(data,
+                           vector_size=vecSize,
+                           minCount=minCount,
+                           workers=4)
     if save:
         saveData(doc2VecModel, 'doc2vec-' + name)
     return doc2VecModel
 
 
-def clusterData(clusterData, labels, save=False, name=""):
-    ''' Run kmeans on given data '''
-    kmeans = KMeans(n_clusters=20).fit(clusterData)
+def trainDoc2Vec(corpus, minCount=1, vecSize=20, save=False, name=""):
+    ''' Train a Doc2Vec model, expects corpus '''
+    data = [TaggedDocument(words=doc, tags=[idx]) for idx, doc in enumerate(corpus)]
+    doc2VecModel = Doc2Vec(data,
+                           vector_size=vecSize,
+                           minCount=minCount,
+                           workers=4)
     if save:
-        saveData(kmeans.labels_, "kmeans-" + name)
-    return kmeans.labels_, normalized_mutual_info_score(kmeans.labels_, labels)
+        saveData(doc2VecModel, 'doc2vec-' + name)
+    return doc2VecModel
 
 
 def clusterDataMiniBatch(clusterData, labels, save=False, name=""):
     ''' Run minibatch kmeans on given data for faster performance '''
     kmeans = MiniBatchKMeans(n_clusters=20).fit(clusterData)
     if save:
-        saveData(kmeans.labels_, "kmeansmb-" + name)
+        saveData(kmeans.labels_, "kmeans-" + name)
     return kmeans.labels_, normalized_mutual_info_score(kmeans.labels_, labels)
+
+
+def getLDARep(ldaModel, docRep, save=False, name=""):
+    ''' Convert doc representation to lda output '''
+    corpus = Sparse2Corpus(docRep, documents_columns=False)
+    converted = ldaModel.get_document_topics(corpus, minimum_probability=0.0)
+    rep = [list(map(lambda topic: topic[1], converted[i]))
+           for i in range(len(corpus))]
+    if save:
+        saveData(rep, name)
+    return rep
+
+
+def getDoc2VecRep(doc2VecModel, save=False, name=""):
+    ''' Convert doc2vec to vector representation '''
+    rep = [doc2VecModel.docvecs[i] for i in range(len(doc2VecModel.docvecs))]
+    if save:
+        saveData(rep, name)
+    return rep
